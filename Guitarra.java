837dcf2a52d15de866e096793d398badcb6df6de
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-16 21:23:58 -0300

    Criação
    Criando as partes da guitarra, e removendo a variável que estava dando erro.

--- Guitarra.java
+++ Guitarra.java
@@ -1,5 +1,17 @@
 public class Guitarra extends Instrumentos{
 
-    private int nCordas;
+    public Guitarra(int nCaptadores, int nCordas, String tom, int tamanho, String tipoCordas) {
+        super(tom, tamanho, tipoCordas);
+        
+    }
+
+    public void tocarSolo(){
+
+        System.out.println("Tocando solo!!");
+    }
     
+    public void afinandoCordas(){
+
+        System.out.println("Afinando todas as cordas da guitarra!!");
+    }
 }
