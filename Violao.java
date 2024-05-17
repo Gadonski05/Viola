a9fbb4d86d4da29e3c32f1a400f34e1c768d5e19
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-16 21:20:52 -0300

    Error
    Removi as variáveis que estavam atrasando o código.

--- Violao.java
+++ Violao.java
@@ -1,16 +1,12 @@
 public class Violao extends Instrumentos{
 
-    private int nCordas;
-
     public Violao(String tom, int tamanho, int nCordas, String tipoCordas){
         super(tom, tamanho, tipoCordas);
-
-        this.nCordas = nCordas;
     }
     
     @Override
 
-    public void Tocar(){
+    public void tocarAcordes(){
 
         System.out.println("Tocando violao!!");
     }
