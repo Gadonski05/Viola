659dc50085c2a869e2d0693b19b04ee0fa4cb712
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-16 21:25:16 -0300

    Infos
    Criação das expecificações do main e das informações dos instrumentos.

--- Main.java
+++ Main.java
@@ -2,11 +2,16 @@ public class Main {
  
     public static void main(String[] args) {
         
-        violao v = new Violao();
-        Flauta f = new Flauta();
+        violao v = new Violao("Suave", 0, 0, "Cobre");
+        Flauta f = new Flauta("Agudo", 0);
+        Guitarra g = new Guitarra(0, 0, null, 0, "Cobre");
 
     }
 
-    v.tocar();
-    f.tocar();
+    v.tocarAcordes();
+    
+
+    f.tocarMelodia();
+
+    g.tocarSolo();
 }
