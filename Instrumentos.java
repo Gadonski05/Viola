aa26433c98b7149e7a76e26a5cb663037dcc75e4
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-05-16 21:26:10 -0300

    Update Instrumentos.java

--- Instrumentos.java
+++ Instrumentos.java
@@ -1,8 +1,6 @@
 public class Instrumentos {
     
     public Instrumentos(String tom, int tamanho, String tipoCordas) {
-        //TODO Auto-generated constructor stub
+       
     }
-
-    private int nCordas;
 }
